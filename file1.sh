1e79d85f8b84d9f7939bc4fb527dc85474ded16f
kranthi318 <chevuru.kranthikumar@gmail.com>
2018-05-01 06:04:22 +0000

    kranthi-details

---
+++ file1.sh
@@ -0,0 +1,13 @@
+clear
+
+
+echo " ---------------welcome ------------ "
+echo " Name       : Ch.KranthiKumar "
+echo " Phone No   : 9848985441 "
+echo " Address(P) : Madamanuru (VI&PO) "
+echo "              Manubolu (MD) "
+echo "              SPSR Nellore "
+echo " Postel Id  : 524405 "
+echo -e "\n\n\n"
+echo " -----Task completed -----"
+echo -e " \n\t Thank you ....\n\n\n"
